473ba29bd40c451ecf9f208881a7a042dfe02519
Mayank-124 <mayanknarang124@gmail.com>
2021-10-19 23:55:08 +0530

    Prototype Added, Yeah !

---
+++ array.c
@@ -0,0 +1,6 @@
+ #include"array.h"
+ void showIdentity()
+ {
+    printf("Hey there, I am an Instance of Array Data Structure !\n");
+    printf("Wait bro, Soon I will add the features to this Instance... ");
+ }
---
+++ array.h
@@ -0,0 +1,6 @@
+#ifndef ARRAY_DS_H
+#define ARRAY_DS_H
+
+#include<stdio.h>
+void showIdentity();
+#endif
